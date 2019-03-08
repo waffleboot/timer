76debfcbe94e55500c14d07e96a5415e233b5d0f
Andrei Yangabishev <waffleboot@gmail.com>
2019-03-08 06:07:33 +0300

    fix formating options

--- main.go
+++ main.go
@@ -13,6 +13,18 @@ import (
 
 type exitRequestError struct{}
 
+type item struct {
+	time time.Duration
+	name string
+}
+
+type term struct {
+	time   time.Time
+	items  []item
+	prompt string
+	line   *liner.State
+}
+
 func (exitRequestError) Error() string {
 	return ""
 }
@@ -23,7 +35,7 @@ func (t *term) call(cmdstr string) error {
 	}
 	s := strings.Fields(cmdstr)
 	switch s[0] {
-	case "e":
+	case "q", "e":
 		return exitRequestError{}
 	case "s":
 		return t.cmdshow()
@@ -31,90 +43,89 @@ func (t *term) call(cmdstr string) error {
 		return t.cmddel(s[1:])
 	case "t":
 		return t.cmdtime(s[1:])
+	default:
+		return errors.New(cmdstr)
 	}
-	return errors.New(cmdstr)
 }
 
-type path struct {
-	time time.Duration
-	name string
-}
-
-type term struct {
-	starttime   time.Time
-	subtraction []path
-	prompt      string
-	line        *liner.State
-}
-
-func formattime(t time.Time) string {
-	return t.Format("15:04")
-}
-
-func formatduration(d time.Duration) string {
-	h := d / time.Hour
-	m := (d - h*time.Hour) / time.Minute
-	return fmt.Sprintf("%02d:%02d", h, m)
-}
-
-func (t *term) cmddel(s []string) error {
-	if len(s) == 0 {
-		t.subtraction = t.subtraction[0:0]
+func (t *term) subtime(s string) error {
+	for _, custom := range customdurations {
+		if s == custom.name {
+			t.additem(custom.dur, custom.desc)
+			return nil
+		}
+	}
+	if s == "" {
+		showCustomDurations()
 		return nil
 	}
-	p, err := strconv.Atoi(s[0])
-	if err != nil || p < 1 || len(t.subtraction) < p {
-		return nil
+	d, desc, err := parseduration(s)
+	if err != nil {
+		return err
+	} else if d != 0 {
+		t.additem(d, desc)
 	}
-	t.subtraction = append(t.subtraction[:p-1], t.subtraction[p:]...)
+	return nil
+}
+
+func (t *term) additem(d time.Duration, s string) {
+	t.items = append(t.items, item{d, s})
 	t.cmdshow()
-	return nil
-}
-
-func (t *term) cmdshow() error {
-	var i int
-	var sum time.Duration
-	if len(t.subtraction) > 0 {
-		for _, s := range t.subtraction {
-			i++
-			fmt.Printf("%v)\t%v %v\n", i, formatduration(s.time), s.name)
-			sum += s.time
-		}
-		fmt.Println("--------------")
-		if !t.starttime.IsZero() {
-			fmt.Printf("%v-->%v (%v)\n", formattime(t.starttime), formattime(t.starttime.Add(-sum)), formatduration(sum))
-		} else {
-			fmt.Printf("total\t%v\n", formatduration(sum))
-		}
-	}
-	return nil
 }
 
 func parseduration(s string) (time.Duration, string, error) {
 	var x, y int
 	a := strings.Fields(s)
 	b := strings.SplitN(a[0], ":", 2)
-	n := ""
+	desc := ""
 	if len(a) > 1 {
-		n = a[1]
+		desc = a[1]
 	}
 	x, err := strconv.Atoi(b[0])
 	if err != nil {
 		return 0, "", err
 	}
 	if len(b) > 1 {
-		y, err = strconv.Atoi(b[1])
-		if err != nil {
-			return 0, "", nil
+		if len(b[1]) > 0 {
+			y, err = strconv.Atoi(b[1])
+			if err != nil {
+				return 0, "", nil
+			}
 		}
-		return time.Duration(x)*time.Hour + time.Duration(y)*time.Minute, n, nil
+		return time.Duration(x)*time.Hour + time.Duration(y)*time.Minute, desc, nil
 	}
-	return time.Duration(x) * time.Minute, n, nil
+	return time.Duration(x) * time.Minute, desc, nil
 }
 
-func (t *term) addtime(d time.Duration, s string) {
-	t.subtraction = append(t.subtraction, path{d, s})
-	t.cmdshow()
+func (t *term) cmddel(s []string) error {
+	if len(s) == 0 {
+		t.items = t.items[:0]
+		fmt.Fprintf(os.Stdout, "all items deleted")
+		return nil
+	}
+	p, err := strconv.Atoi(s[0])
+	if err != nil || p < 1 || len(t.items) < p {
+		return nil
+	}
+	t.items = append(t.items[:p-1], t.items[p:]...)
+	return t.cmdshow()
+}
+
+func (t *term) cmdshow() error {
+	var sum time.Duration
+	if len(t.items) > 0 {
+		for i, s := range t.items {
+			fmt.Printf("%v)\t%v %v\n", i+1, formatduration(s.time), s.name)
+			sum += s.time
+		}
+		fmt.Println("--------------")
+		if !t.time.IsZero() {
+			fmt.Printf("%v-->%v (%v)\n", formattime(t.time), formattime(t.time.Add(-sum)), formatduration(sum))
+		} else {
+			fmt.Printf("total\t%v\n", formatduration(sum))
+		}
+	}
+	return nil
 }
 
 var customdurations []struct {
@@ -135,51 +146,23 @@ func init() {
 	}
 }
 
-func (t *term) subtime(s string) error {
-	for _, custom := range customdurations {
-		if s == custom.name {
-			t.addtime(custom.dur, custom.desc)
-			return nil
+func completeCustomDuration(t string) []string {
+	if len(t) > 0 {
+		s := strings.Fields(t)[0]
+		for _, custom := range customdurations {
+			if strings.HasPrefix(custom.name, s) {
+				return []string{"-" + custom.name}
+			}
 		}
-	}
-	if s == "" {
-		fmt.Fprintf(os.Stderr, "custom durations:\n")
-		for _, d := range customdurations {
-			fmt.Fprintf(os.Stderr, "-%-20s\t%s\n", d.name, d.desc)
-		}
-		return nil
-	}
-	d, n, err := parseduration(s)
-	if err != nil {
-		return err
-	} else if d != 0 {
-		t.addtime(d, n)
 	}
 	return nil
 }
 
-func parsetime2(s []string) (time.Time, error) {
-	if len(s) == 1 {
-		return time.Parse("15", s[0])
+func showCustomDurations() {
+	fmt.Fprintf(os.Stdout, "custom durations:\n")
+	for _, d := range customdurations {
+		fmt.Fprintf(os.Stdout, "-%-20s\t%s\n", d.name, d.desc)
 	}
-	return time.Parse("15:04", s[0]+":"+s[1])
-}
-
-func parsetime(s []string) (time.Time, error) {
-	n, err := time.Parse("15:04", s[0])
-	if err != nil {
-		return parsetime2(s)
-	}
-	return n, nil
-}
-
-func (t *term) settime(s []string) error {
-	n, err := parsetime(s)
-	if err != nil {
-		return err
-	}
-	t.starttime = n
-	return nil
 }
 
 func (t *term) cmdtime(s []string) error {
@@ -190,6 +173,37 @@ func (t *term) cmdtime(s []string) error {
 	return nil
 }
 
+func (t *term) settime(s []string) error {
+	time, err := parsehhmm(s)
+	if err != nil {
+		return err
+	}
+	t.time = time
+	return nil
+}
+
+func parsehhmm(s []string) (time.Time, error) {
+	time, err := time.Parse("15:04", s[0])
+	if err != nil {
+		return parsehh(s)
+	}
+	return time, err
+}
+
+func parsehh(s []string) (time.Time, error) {
+	if len(s) == 1 {
+		t, err := time.Parse("15", s[0])
+		if err != nil {
+			return time.Parse("15:", s[0])
+		} else {
+			return t, err
+		}
+	}
+	return time.Parse("15:04", s[0]+":"+s[1])
+}
+
+//-----------------------------------------------------------------------------
+
 func newTerm() *term {
 	return &term{
 		prompt: "$ ",
@@ -197,32 +211,11 @@ func newTerm() *term {
 	}
 }
 
-func usage() {
-	fmt.Fprintf(os.Stderr, `		commands:
-[e]xit
-[s]how		show full timetable
-[d]elete n	delete n-th item
-[t]ime		set timepoint
--		show custom durations
--mm		add time interval
--hh:mm		add time interval
--name		add custom interval
-`)
-}
-
 func (t *term) Run() {
 	defer t.close()
 	t.line.SetCompleter(func(line string) []string {
 		if strings.HasPrefix(line, "-") {
-			t := strings.TrimSpace(line[1:])
-			if len(t) > 0 {
-				s := strings.Fields(t)[0]
-				for _, custom := range customdurations {
-					if strings.HasPrefix(custom.name, s) {
-						return []string{"-" + custom.name}
-					}
-				}
-			}
+			return completeCustomDuration(strings.TrimSpace(line[1:]))
 		}
 		return nil
 	})
@@ -239,6 +232,21 @@ func (t *term) Run() {
 			fmt.Fprintf(os.Stderr, "command failed: %s\n", err)
 		}
 	}
+}
+
+func usage() {
+	fmt.Fprintf(os.Stdout, `		commands:
+[q]uit
+[e]xit
+[s]how		show full timetable
+[d]elete	delete all items
+[d]elete n	delete n-th item
+[t]ime hh:mm	set timepoint
+-		show custom durations
+-mm		add time interval
+-hh:mm		add time interval
+-name		add custom interval
+`)
 }
 
 func (t *term) promptForInput() (string, error) {
@@ -258,6 +266,20 @@ func (t *term) close() {
 }
 
 func main() {
-	fmt.Fprintf(os.Stderr, "$ press Enter to show usage\n")
+	fmt.Fprintf(os.Stdout, "$ press Enter to show usage\n")
 	newTerm().Run()
+}
+
+func formattime(t time.Time) string {
+	return t.Format("15:04")
+}
+
+func formatduration(d time.Duration) string {
+	h := d / time.Hour
+	m := (d - h*time.Hour) / time.Minute
+	if h == 0 {
+		return fmt.Sprintf("% 2d:%02d", h, m)
+	} else {
+		return fmt.Sprintf("%02d:%02d", h, m)
+	}
 }
