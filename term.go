1fcf00cc587bb05cfd8f0543af40611eccac8429
Andrei Yangabishev <waffleboot@gmail.com>
2021-09-04 16:34:45 +0300

    rename line to liner

--- term.go
+++ term.go
@@ -7,16 +7,16 @@ import (
 )
 
 type term struct {
-	line   *liner.State
+	liner  *liner.State
 	prompt string
 }
 
 func newTerm() *term {
 	t := &term{
-		line:   liner.NewLiner(),
+		liner:  liner.NewLiner(),
 		prompt: "$ ",
 	}
-	t.line.SetCompleter(func(line string) []string {
+	t.liner.SetCompleter(func(line string) []string {
 		if strings.HasPrefix(line, "-") {
 			return completeCustomDuration(strings.TrimSpace(line[1:]))
 		}
@@ -30,17 +30,17 @@ func (t *term) read() (string, error) {
 }
 
 func (t *term) promptForInput() (string, error) {
-	l, err := t.line.Prompt(t.prompt)
+	l, err := t.liner.Prompt(t.prompt)
 	if err != nil {
 		return "", err
 	}
 	l = strings.TrimSuffix(l, "\n")
 	if l != "" {
-		t.line.AppendHistory(l)
+		t.liner.AppendHistory(l)
 	}
 	return l, nil
 }
 
 func (t *term) close() {
-	t.line.Close()
+	t.liner.Close()
 }
